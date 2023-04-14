508c3f384de0d39c6e03454be724cb4e1bdb7ba5
Elvis Cantaderio <vc20009@ues.edu.sv>
2023-04-13 22:42:17 -0600

    arreglos en js

---
+++ hola.js
@@ -0,0 +1,10 @@
+var arreglo=['hola','mundo','cruel'];
+for (let index = 0; index < arreglo.length; index++) {
+    const element = arreglo[index];
+    console.log(element);
+    
+}
+arreglo.map(hola => {
+    console.log(hola);
+});
+
